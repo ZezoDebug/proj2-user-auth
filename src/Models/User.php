eb1a148fe8485aee680f5885bdd9f121e532139a
ZezoDebug <josefaculdade.24@gmail.com>
2025-09-19 02:01:31 -0300

    feat: criar classe User com propriedades privadas e construtor

---
+++ src/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/Data/users.php';
+
+class User
+{
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $password; 
+
+    public function __construct(int $id, string $name, string $email, string $password) 
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->setPassword($password);
+    }
+}
