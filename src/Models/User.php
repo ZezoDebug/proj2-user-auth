13a4d0ef329589cb92cf0ec75f77d1e72bbe1b6b
ZezoDebug <josefaculdade.24@gmail.com>
2025-09-19 13:20:43 -0300

    feat: criar classes User e Validator com validaçãode email e senha

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/Data/users.php';
+require_once __DIR__ . '/Utils/Validator.php';
 
 class User
 {
--- src/Utils/Validator.php
+++ src/Utils/Validator.php
@@ -1,1 +1,35 @@
 <?php
+
+declare(strict_types=1);
+
+class Validator
+{
+    # Validação do Email
+    public function isValidEmail(string $email): bool
+    {
+        if (empty($email)){
+            return false;
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function isStrongPassword(string $password): bool
+    {
+        if (empty($password)){
+            return false;
+        } elseif (strlen($password) < 8 ) {
+            return false;
+        } elseif (!preg_match('/[A-Z]/', $password)) {
+            return false;
+        } elseif (!preg_match('/[a-z]/', $password)) {
+            return false;
+        } elseif (!preg_match('/[0-9]/', $password)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}           
